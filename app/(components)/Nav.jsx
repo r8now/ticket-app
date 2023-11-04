824f6a278ed79293e8ace77a497dd67e50f1674b
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-11-04 11:51:22 +0100

    Add navigation links and email address to Nav component, update layout to include Nav and adjust page content display

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,8 +1,26 @@
+import { faHome,faTicket } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <div>
+        <Link href={"/"}>
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href={"/TicketPage/new"}>
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+        <Link href={"/"}>
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text">email.com</p>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav
